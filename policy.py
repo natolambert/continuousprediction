ec6394c8840aa68e40179a721dbfb712a606ae41
Nathan Lambert <nol@fb.com>
2019-09-27 10:08:41 -0700

    init

---
+++ policy.py
@@ -0,0 +1,226 @@
+# Compatibility Python 2/3
+from __future__ import division, print_function, absolute_import
+from builtins import range
+# ----------------------------------------------------------------------------------------------------------------------
+
+import sys
+import numpy as np
+import copy
+from datetime import datetime
+import R.log as rlog
+from timeit import default_timer as timer
+
+
+class Policy(object):
+    def __init__(self, dX, dU, actionBounds=None):
+        """
+
+        :param dX: scalar. dimensionality of the state
+        :param dU: scalar. dimensionality of the control signal
+        :param actionBounds: type R.utils.bounds
+        """
+        self.dX = dX
+        self.dU = dU
+        self.bounds = actionBounds
+
+    def act(self, x, obs=None, time=None, noise=None):
+        """
+        Function to be called externally. Compute next action.
+        :param x:
+        :param obs:
+        :param time:
+        :param noise:
+        :return:
+            a: action to perform
+            t: scalar amount of time used to compute action
+        """
+        start = timer()
+        x = np.squeeze(x)
+        assert x.shape[0] is self.dX, 'Wrong dimension state %d, should be %d' % (x.shape[0], self.dX)
+        a = self._action(x, obs, time, noise)  # Compute action
+        if a.ndim > 0:  # TODO: deal with the ndim==0 case
+            assert a.shape[0] is self.dU, 'Wrong dimension control %d, should be %d' % (a.shape[0], self.dU)
+        if self.bounds is not None:  # Bound actions, if desired
+            a = np.maximum(np.minimum(a, self.bounds.get_max()), self.bounds.get_min())
+        t = (timer() - start)
+        return a, t
+
+    def _action(self, x, obs, time, noise):
+        """
+        Abstract internal interface to compute action
+        :param x:
+        :param obs:
+        :param time:
+        :param noise:
+        :return:
+        """
+        raise NotImplementedError('Implement in subclass')
+
+
+class randomPolicy(Policy):
+
+    def __init__(self, dX, dU, variance=1):
+        Policy.__init__(self, dX=dX, dU=dU)
+        self.variance = variance
+
+    def _action(self, x, obs, time, noise):
+        action = self.variance * np.random.rand(self.dU)
+        return action
+
+
+class sequenceActions(Policy):
+    """
+    This policy simply execute a pre-defined sequence of actions
+    """
+    def __init__(self, dX, dU, actions):
+        Policy.__init__(self, dX=dX, dU=dU)
+        self._actions = np.matrix(actions)
+        self.n_timestep = self._actions.shape[0]
+        assert self._actions.shape[1] == dU, 'Incorrect size of actions wrt n_dofs'
+        self._counter = 0
+        # assert "action_list" in self._data, "Incorrect/missing parameters for initialization."
+
+    def _action(self, x, obs, time, noise):
+        """
+        Returns the action an agent following this policy would
+        take, given a STATE, OBSERVATION, and TIMESTEP.
+
+        Note: if time_step >= TIMEHORIZON, this function
+        returns an action that does nothing.
+        """
+        # TODO: use time instead of _counter ?
+        out = self._actions[min(self.n_timestep-1, self._counter), :]
+        out = out.ravel()
+        self._counter += 1
+        return out
+
+
+class linearController(Policy):
+    def __init__(self, dX, dU, A, B):
+        """
+        linear controller of the form A*x + B
+        :param n_dof:
+        :param A:
+        :param B:
+        """
+        Policy.__init__(dX=dX, dU=dU)
+        self.A = np.matrix(A)
+        self.B = np.matrix(B)
+        if self.A.ndim is 3:
+            self.timevariant = True
+        else:
+            self.timevariant = False
+
+    def _action(self, x, obs, time, noise):
+        if self.timevariant is True:
+            return self.A[time, :, :] * x + self.B[time, :]
+        else:
+            return self.A * x + self.B
+
+
+class PID(Policy):
+
+    def __init__(self, dX, dU, P, I, D):
+        Policy.__init__(self, dX=dX, dU=dU)
+        self.n_dof = dU
+        # TODO: fix dimensionality with P
+        if isinstance(P, int):
+            self.Kp = np.tile(P, self.n_dof)
+        else:
+            self.Kp = P
+        if isinstance(I, int):
+            self.Ki = np.tile(I, self.n_dof)
+        else:
+            self.Ki = I
+        if isinstance(D, int):
+            self.Kd = np.tile(D, self.n_dof)
+        else:
+            self.Kd = D
+        self.prev_error = 0
+        self.error = 0
+
+    def _action(self, q, q_des):
+        self.error = q_des - q
+        P_value = self.Kp * self.error
+        I_value = 0  # TODO: implement I and D part
+        D_value = self.Kd * (self.error - self.prev_error)  # + self.D*(qd_des-qd)
+        self.prev_error = self.error
+        action = P_value + I_value + D_value
+        return action
+
+    def get_P(self):
+        return self.Kp
+
+    def get_I(self):
+        return self.Ki
+
+    def get_D(self):
+        return self.Kd
+
+
+class jointsTrajectoryTrackingPID(PID):
+    """
+    jointsTrajectoryTrackingPID
+    """
+    def __init__(self, dX, dU, trajectory, P=1, I=0, D=0, id_states=np.arange(7), verbosity=1):
+        """
+
+        :param dX:
+        :param dU:
+        :param trajectory:
+        :param P:
+        :param I:
+        :param D:
+        :param id_states: numpy array with the index of the elements of dX that will be used in the controller
+        :param verbosity:
+        """
+        PID.__init__(self, dX=dX, dU=dU, P=P, I=I, D=D)
+        if trajectory.ndim is 1:
+            # Single point
+            self.trajectory = np.expand_dims(trajectory, axis=0)
+        else:
+            self.trajectory = trajectory
+        assert self.trajectory.shape[1] == self.n_dof, \
+            'Number of DOF in the trajectory does not match the number of DOF specified: %d != %d' % (self.trajectory.shape[1], self.n_dof)
+        self.n_timestep = self.trajectory.shape[0]
+        self.id_states = id_states
+        self.verbosity = verbosity
+
+    def _action(self, x, obs, time, noise):
+        q = x[self.id_states]  # Only care about the position
+        if time >= self.n_timestep:
+            # Keep the controller stable at the last position
+            action = PID._action(self, q, self.trajectory[self.n_timestep-1, :])
+        else:
+            action = PID._action(self, q, self.trajectory[time, :])
+            # TODO: use min(self.n_timestep-1, time)
+        if self.verbosity > 1:
+            print(action)
+        return action
+
+# class LQG(Policy):
+#     def __init__(self, n_dof, A, B, Q, R, horizon=10):
+#         '''
+#
+#         :param n_dof:
+#         :param trajectory:
+#         :param horizon: Horizon
+#         '''
+#         Policy.__init__(self, n_dof=n_dof)
+#         self.T = horizon
+#         self.A = A
+#         self.B = B
+#         self.Q = Q
+#         self.R = R
+#         self.controller = self.compute_controller()
+#
+#     def compute_controller(self):
+#         # TODO: implement me!!!
+#         K = 0
+#         k = 0
+#         controller = linearController(A=K, B=k)
+#         return 0
+#
+#     def _action(self, x, obs, time, noise):
+#         return self.controller.action(x, obs, time, noise)
+
---
+++ reacher_pd.py
@@ -0,0 +1,404 @@
+# Compatibility Python 2/3
+from __future__ import division, print_function, absolute_import
+from builtins import range
+from past.builtins import basestring
+# ----------------------------------------------------------------------------------------------------------------------
+
+import numpy as np
+from dotmap import DotMap
+# import matplotlib.pyplot as plt
+from matplotlib.pyplot import cm
+
+# import R.data as rdata
+import progressbar
+from timeit import default_timer as timer
+import matplotlib.pyplot as plt
+# import scipyplot as spp
+
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+import save
+import gym
+# from gym.monitoring import VideoRecorder
+import src.env
+import scipyplot as spp
+
+import logging
+logging.basicConfig(
+    filename="test.log",
+    level=logging.DEBUG,
+    format="%(asctime)s:%(levelname)s:%(message)s"
+    )
+# logger = logging.getLogger()
+# logger.setLevel(logging.DEBUG)
+# # To log file
+# fh = logging.FileHandler('example.log')
+# fh.setLevel(logging.DEBUG)
+# logger.addHandler(fh)
+
+from policy import PID
+
+
+def stateAction2forwardDyn(states, actions):
+    data_in = np.concatenate((states[:-1, :], actions[:-1, :]), axis=1)
+    data_out = states[1:, :]
+    return [data_in, data_out]
+
+
+class Net(nn.Module):
+    """
+    Neural Network
+    """
+    def __init__(self, structure=[20, 100, 100, 1], tf=F.relu):
+        super(Net, self).__init__()
+
+        # TODO: parameteric NN
+        # self.fc = []
+        self.n_layers = len(structure)-1
+        # for idx in range(self.n_layers):
+        #     self.fc[idx] = nn.Linear(structure[idx], structure[idx+1])
+        self.fc1 = nn.Linear(structure[0], structure[1])
+        self.fc2 = nn.Linear(structure[1], structure[2])
+        self.fc3 = nn.Linear(structure[2], structure[3])
+        self.tf = tf
+        self._onGPU = False
+
+    def forward(self, x):
+        x = self.tf(self.fc1(x))
+        x = self.tf(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    def predict(self, x):
+        """
+        wrapper to/from numpy array
+        :param x:
+        :return:
+        """
+        if self._onGPU:
+            pass
+        else:
+            return self.forward(Variable(torch.from_numpy(np.matrix(x)).float())).data.cpu().numpy()
+
+
+# class FaceLandmarksDataset(Dataset):
+#     """Face Landmarks dataset."""
+#
+#     def __init__(self, csv_file, root_dir, transform=None):
+#         """
+#         Args:
+#             csv_file (string): Path to the csv file with annotations.
+#             root_dir (string): Directory with all the images.
+#             transform (callable, optional): Optional transform to be applied
+#                 on a sample.
+#         """
+#         self.landmarks_frame = pd.read_csv(csv_file)
+#         self.root_dir = root_dir
+#         self.transform = transform
+#
+#     def __len__(self):
+#         return len(self.landmarks_frame)
+#
+#     def __getitem__(self, idx):
+#         img_name = os.path.join(self.root_dir,
+#                                 self.landmarks_frame.iloc[idx, 0])
+#         image = io.imread(img_name)
+#         landmarks = self.landmarks_frame.iloc[idx, 1:].as_matrix()
+#         landmarks = landmarks.astype('float').reshape(-1, 2)
+#         sample = {'image': image, 'landmarks': landmarks}
+#
+#         if self.transform:
+#             sample = self.transform(sample)
+#
+#         return sample
+
+def train_network(dataset, model, parameters=DotMap()):
+    import torch.optim as optim
+
+    p = DotMap()
+    p.opt.n_epochs = parameters.get('n_epochs', 10)
+    p.opt.optimizer = optim.Adam
+    p.opt.batch_size = parameters.get('batch_size', 100)
+    p.criterion = nn.MSELoss()
+    p.learning_rate = parameters.get('learning_rate', 0.0001)
+    p.useGPU = parameters.get('useGPU', False)
+    p.verbosity = parameters.get('verbosity', 1)
+    p.logs = parameters.get('logs', None)
+
+    # Init logs
+    if p.logs is None:
+        logs = DotMap()
+        logs.training_error = []
+        logs.time = None
+    else:
+        logs = p.logs
+
+    # Optimizer
+    optimizer = p.opt.optimizer(model.parameters(), lr=p.learning_rate)
+
+    cudnn.benchmark = True
+
+    from torch.utils.data.dataset import Dataset
+    from torch.utils.data import DataLoader
+    from torch.utils.data.sampler import Sampler
+
+    if p.useGPU:
+        model.cuda()
+        p.criterion.cuda()
+
+    class PytorchDataset(Dataset):
+        def __init__(self, dataset):
+            self.inputs = torch.from_numpy(dataset[0]).float()
+            self.outputs = torch.from_numpy(dataset[1]).float()
+            self.n_data = dataset[0].shape[0]
+            self.n_inputs = dataset[0].shape[1]
+            self.n_outputs = dataset[1].shape[1]
+
+        def __getitem__(self, index):
+            # print('\tcalling Dataset:__getitem__ @ idx=%d' % index)
+            input = self.inputs[index]
+            output = self.outputs[index]
+            return input, output
+
+        def __len__(self):
+            # print('\tcalling Dataset:__len__')
+            return self.n_data
+
+
+    logging.info('Training NN from dataset')
+
+    dataset = PytorchDataset(dataset=dataset)
+    loader = DataLoader(dataset, batch_size=p.opt.batch_size, shuffle=True)  ##shuffle=True #False
+        # pin_memory=True
+        # drop_last=False
+
+    startTime = timer()
+    if logs.time is None:
+        logs.time = [0]
+
+    for epoch in range(p.opt.n_epochs):
+        for i, data in enumerate(loader, 0):
+            # Load data
+            inputs, targets = data
+            if p.useGPU:
+                inputs = Variable(inputs.cuda())
+                targets = Variable(targets.cuda())
+            else:
+                inputs = Variable(inputs)
+                targets = Variable(targets)
+
+            optimizer.zero_grad()
+            outputs = model.forward(inputs)
+            loss = p.criterion(outputs, targets)
+
+            e = loss.data[0]
+            logs.training_error.append(e)
+            logging.info('Iter %010d - %f ' % (epoch, e))
+            loss.backward()
+            optimizer.step()  # Does the update
+            logs.time.append(timer() - logs.time[-1])
+
+    endTime = timer()
+    logging.info('Optimization completed in %f[s]' % (endTime - startTime))
+
+    return model.cpu(), logs
+
+
+def plot_pred(groundtruth, prediction, sorted=True):
+    plt.figure()
+    if sorted:
+        gt = groundtruth.sort()
+    else:
+        gt = groundtruth
+    plt.plot(gt)
+    plt.plot(prediction)
+    plt.show()
+
+
+def run_controller(env, horizon, policy):
+    def obs2q(obs):
+        return obs[0:7]
+
+    logs = DotMap()
+    logs.states = []
+    logs.actions = []
+    logs.rewards = []
+    logs.times = []
+
+    observation = env.reset()
+    for t in range(horizon):
+        # env.render()
+        state = observation
+        # print(state)
+        action = policy.act(obs2q(state))
+
+        observation, reward, done, info = env.step(action)
+
+        # Log
+        # logs.times.append()
+        logs.actions.append(action)
+        logs.rewards.append(reward)
+        logs.states.append(observation)
+
+    # Cluster state
+    logs.actions = np.array(logs.actions)
+    logs.rewards = np.array(logs.rewards)
+    logs.states = np.array(logs.states)
+    return logs
+
+
+def collect_data(nTrials=20, horizon=1000):
+    """
+    Collect data
+    :param nTrials:
+    :param horizon:
+    :return:
+    """
+    # env = gym.make('Reacher-v2')
+    env_model = 'Reacher3D-v0'
+    env = gym.make(env_model)
+    logging.info('Initializing env: %s' % env_model)
+
+    logs = []
+
+    # def init_env(env):
+    #     qpos = np.copy(env.init_qpos)
+    #     qvel = np.copy(env.init_qvel)
+    #     qpos[0:7] += np.random.normal(loc=0.5, scale=1, size=[7])
+    #     # qpos[-3:] += np.random.normal(loc=0, scale=1, size=[3])
+    #     # qvel[-3:] = 0
+    #     env.goal = qpos[-3:]
+    #     env.set_state(qpos, qvel)
+    #     env.T = 0
+    #     return env
+
+    for i in range(nTrials):
+        logging.info('Trial %d' % i)
+        env.seed(i)
+        P = np.array([4, 4, 1, 1, 1, 1, 2])
+        I = np.zeros(7)
+        D = [0.2, 0.2, 2, 0.4, 0.4, 0.1, 0.5]
+        target = [0.5, 0.5, 0.5, 0.5, 0.5, 0.2, 0.5]
+        policy = PID(dX=7, dU=7, P=P, I=I, D=D, target=target)
+        logs.append(run_controller(env, horizon=horizon, policy=policy))
+
+        # # Visualize
+        # plt.figure()
+        # h = plt.plot(logs[i].states[:, 0:7])
+        # plt.legend(h)
+        # plt.show()
+    return logs
+
+
+# Learn model t only
+def create_dataset_t_only(states):
+    data_in = []
+    data_out = []
+    for i in range(states.shape[0]):
+        for j in range(i+1, states.shape[0]):
+            data_in.append(np.hstack((states[i], j-i)))
+            data_out.append(states[j])
+    data_in = np.array(data_in)
+    data_out = np.array(data_out)
+    return data_in, data_out
+
+
+def create_dataset(data):
+    dataset_in = None
+    dataset_out = None
+    for sequence in data:
+        inputs, outputs = create_dataset_t_only(sequence.states)
+        if dataset_in is None:
+            dataset_in = inputs
+            dataset_out = outputs
+        else:
+            dataset_in = np.concatenate((dataset_in, inputs), axis=0)
+            dataset_out = np.concatenate((dataset_out, outputs), axis=0)
+    return [dataset_in, dataset_out]
+
+
+def main():
+
+    COLLECT_DATA = False
+    CREATE_DATASET = False
+    TRAIN_MODEL = False
+
+    # Collect data
+    if collect_data():
+        logging.info('Collecting data')
+        train_data = collect_data(nTrials=1)  # 50
+        test_data = collect_data(nTrials=1)  # 5
+    else:
+        pass
+
+    # Create dataset
+    if CREATE_DATASET:
+        logging.info('Creating dataset')
+        dataset = create_dataset(train_data)
+    else:
+        pass
+
+    # Train model
+    model_file = 'model.pth.tar'
+    n_in = dataset[0].shape[1]
+    n_out = dataset[1].shape[1]
+    model = Net(structure=[n_in, 2000, 2000, n_out])
+    if TRAIN_MODEL:
+        p = DotMap()
+        p.opt.n_epochs = 1  # 1000
+        p.learning_rate = 0.000001
+        p.useGPU = True
+        model, logs = train_network(dataset=dataset, model=model, parameters=p)
+        logging.info('Saving model to file: %s' % model_file)
+        torch.save(model.state_dict(), model_file)
+        save.save(logs, 'logs.pkl')
+        # TODO: save logs to file
+    else:
+        logging.info('Loading model to file: %s' % model_file)
+        checkpoint = torch.load(model_file)
+        if isinstance(checkpoint, dict) and 'state_dict' in checkpoint:
+            model.load_state_dict(checkpoint['state_dict'])
+        else:
+            model.load_state_dict(checkpoint)
+        logs = save.load('logs.pkl')
+        # TODO: load logs from file
+
+    # # Plot optimization NN
+    # plt.figure()
+    # plt.plot(np.array(logs.training_error))
+
+    if False:
+        # Evaluate learned model
+        def augment_state(state, horizon=990):
+            """
+            Augment state by including time
+            :param state:
+            :param horizon:
+            :return:
+            """
+            out = []
+            for i in range(horizon):
+                out.append(np.hstack((state, i)))
+            return np.array(out)
+
+        idx_trajectory = 0
+        idx_state = 2
+        state = test_data[idx_trajectory].states[idx_state]
+        remaining_horizon = test_data[idx_trajectory].states.shape[0] - idx_state - 1
+        groundtruth = test_data[idx_trajectory].states[idx_state:]
+        pred_out = np.concatenate((state[None, :], model.predict(augment_state(state, horizon=remaining_horizon))))
+        idx_plot = range(7)
+        for i in idx_plot:
+            plt.figure()
+            h1 = plt.plot(pred_out[:, i], label='Prediction')
+            h2 = plt.plot(groundtruth[:, i], c='r', label='Groundtruth')
+            plt.legend()
+            plt.show()
+
+
+if __name__ == '__main__':
+    main()
+
