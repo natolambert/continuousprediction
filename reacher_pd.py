5fefc5d7fc8dc181f7463732fe3a3e26463da3bd
Albert Wilcox <albertwilcoxiii@gmail.com>
2019-12-25 08:54:55 -0800

    reorganized the code

--- reacher_pd.py
+++ reacher_pd.py
@@ -27,122 +27,9 @@ log = logging.getLogger(__name__)
 from policy import PID
 from mbrl_resources import *
 
-
-def stateAction2forwardDyn(states, actions):
-    data_in = np.concatenate((states[:-1, :], actions[:-1, :]), axis=1)
-    data_out = states[1:, :]
-    return [data_in, data_out]
-
-
-def plot_pred(groundtruth, prediction, sorted=True):
-    plt.figure()
-    if sorted:
-        gt = groundtruth.sort()
-    else:
-        gt = groundtruth
-    plt.plot(gt)
-    plt.plot(prediction)
-    plt.show()
-
-
-def run_controller(env, horizon, policy):
-    """
-    :param env: A gym object
-    :param horizon: The number of states forward to look
-    :param policy: A policy object (see other python file)
-    """
-
-    # WHat is going on here?
-    def obs2q(obs):
-        return obs[0:5]
-
-    logs = DotMap()
-    logs.states = []
-    logs.actions = []
-    logs.rewards = []
-    logs.times = []
-
-    observation = env.reset()
-    for t in range(horizon):
-        # env.render()
-        state = observation
-        action, t = policy.act(obs2q(state))
-
-        # print(action)
-
-        observation, reward, done, info = env.step(action)
-
-        # Log
-        # logs.times.append()
-        logs.actions.append(action)
-        logs.rewards.append(reward)
-        logs.states.append(observation)
-
-    # Cluster state
-    logs.actions = np.array(logs.actions)
-    logs.rewards = np.array(logs.rewards)
-    logs.states = np.array(logs.states)
-    return logs
-
-
-def collect_data(nTrials=20, horizon=150): # Creates horizon^2/2 points
-    """
-    Collect data for environment model
-    :param nTrials:
-    :param horizon:
-    :return: an array of DotMaps, where each DotMap contains info about a sequence of steps
-    """
-    env_model = 'Reacher3d-v2'
-    env = gym.make(env_model)
-    log.info('Initializing env: %s' % env_model)
-
-    # Logs is an array of dotmaps, each dotmap contains 2d np arrays with data
-    # about <horizon> steps with actions, rewards and states
-    logs = []
-
-    # def init_env(env):
-    #     qpos = np.copy(env.init_qpos)
-    #     qvel = np.copy(env.init_qvel)
-    #     qpos[0:7] += np.random.normal(loc=0.5, scale=1, size=[7])
-    #     qpos[-3:] += np.random.normal(loc=0, scale=1, size=[3])
-    #     qvel[-3:] = 0
-    #     env.goal = qpos[-3:]
-    #     env.set_state(qpos, qvel)
-    #     env.T = 0
-    #     return env
-
-    for i in range(nTrials):
-        log.info('Trial %d' % i)
-        env.seed(i)
-        s0 = env.reset()
-
-        # P = np.array([4, 4, 1, 1, 1])
-        P = np.random.rand(5)*5
-        I = np.zeros(5)
-        # D = np.array([0.2, 0.2, 2, 0.4, 0.4])
-        D = np.random.rand(5)
-
-        # Samples target uniformely from [-1, 1]
-        target = np.random.rand(5)*2-1
-
-        policy = PID(dX=5, dU=5, P=P, I=I, D=D, target=target)
-        # print(type(env))
-
-        dotmap = run_controller(env, horizon=horizon, policy=policy)
-        dotmap.target = target
-        dotmap.P = P
-        dotmap.I = I
-        dotmap.D = D
-        logs.append(dotmap)
-        # print("end pos is: ", logs[i].states[-1, -3:])
-        # # Visualize
-        # plt.figure()
-        # # h = plt.plot(logs[i].states[:, 0:7])
-        # h = plt.plot(logs[i].states[:, -3:])
-        # plt.legend(h)
-        # plt.show()
-    return logs
-
+###########################################
+#            Model Training               #
+###########################################
 
 # Learn model t only
 # Creating a dataset for learning different T values
@@ -217,6 +104,102 @@ def create_dataset_no_t(data):
 
     return data_in, data_out
 
+def run_controller(env, horizon, policy):
+    """
+    :param env: A gym object
+    :param horizon: The number of states forward to look
+    :param policy: A policy object (see other python file)
+    """
+
+    # WHat is going on here?
+    def obs2q(obs):
+        return obs[0:5]
+
+    logs = DotMap()
+    logs.states = []
+    logs.actions = []
+    logs.rewards = []
+    logs.times = []
+
+    observation = env.reset()
+    for t in range(horizon):
+        # env.render()
+        state = observation
+        action, t = policy.act(obs2q(state))
+
+        # print(action)
+
+        observation, reward, done, info = env.step(action)
+
+        # Log
+        # logs.times.append()
+        logs.actions.append(action)
+        logs.rewards.append(reward)
+        logs.states.append(observation)
+
+    # Cluster state
+    logs.actions = np.array(logs.actions)
+    logs.rewards = np.array(logs.rewards)
+    logs.states = np.array(logs.states)
+    return logs
+
+def collect_data(nTrials=20, horizon=150): # Creates horizon^2/2 points
+    """
+    Collect data for environment model
+    :param nTrials:
+    :param horizon:
+    :return: an array of DotMaps, where each DotMap contains info about a sequence of steps
+    """
+    env_model = 'Reacher3d-v2'
+    env = gym.make(env_model)
+    log.info('Initializing env: %s' % env_model)
+
+    # Logs is an array of dotmaps, each dotmap contains 2d np arrays with data
+    # about <horizon> steps with actions, rewards and states
+    logs = []
+
+    # def init_env(env):
+    #     qpos = np.copy(env.init_qpos)
+    #     qvel = np.copy(env.init_qvel)
+    #     qpos[0:7] += np.random.normal(loc=0.5, scale=1, size=[7])
+    #     qpos[-3:] += np.random.normal(loc=0, scale=1, size=[3])
+    #     qvel[-3:] = 0
+    #     env.goal = qpos[-3:]
+    #     env.set_state(qpos, qvel)
+    #     env.T = 0
+    #     return env
+
+    for i in range(nTrials):
+        log.info('Trial %d' % i)
+        env.seed(i)
+        s0 = env.reset()
+
+        # P = np.array([4, 4, 1, 1, 1])
+        P = np.random.rand(5)*5
+        I = np.zeros(5)
+        # D = np.array([0.2, 0.2, 2, 0.4, 0.4])
+        D = np.random.rand(5)
+
+        # Samples target uniformely from [-1, 1]
+        target = np.random.rand(5)*2-1
+
+        policy = PID(dX=5, dU=5, P=P, I=I, D=D, target=target)
+        # print(type(env))
+
+        dotmap = run_controller(env, horizon=horizon, policy=policy)
+        dotmap.target = target
+        dotmap.P = P
+        dotmap.I = I
+        dotmap.D = D
+        logs.append(dotmap)
+        # print("end pos is: ", logs[i].states[-1, -3:])
+        # # Visualize
+        # plt.figure()
+        # # h = plt.plot(logs[i].states[:, 0:7])
+        # h = plt.plot(logs[i].states[:, -3:])
+        # plt.legend(h)
+        # plt.show()
+    return logs
 
 def train_model(dataset, model, model_file, cfg, n_epochs=False, save=True):
     p = DotMap()
@@ -229,8 +212,105 @@ def train_model(dataset, model, model_file, cfg, n_epochs=False, save=True):
         torch.save(model.state_dict(), model_file)
     return model, logs
 
-        # logs = save.load('logs.pkl')
-        # TODO: load logs from file
+
+
+###########################################
+#               Plotting                  #
+###########################################
+def plot_pred(groundtruth, prediction, sorted=True):
+    plt.figure()
+    if sorted:
+        gt = groundtruth.sort()
+    else:
+        gt = groundtruth
+    plt.plot(gt)
+    plt.plot(prediction)
+    plt.show()
+
+def plot_states(ground_truth, prediction_param, prediction_step, idx_plot=None, save=False):
+    num = np.shape(ground_truth)[0]
+    dx = np.shape(ground_truth)[1]
+    if idx_plot is None:
+        idx_plot = list(range(dx))
+
+    for i in idx_plot:
+        gt = ground_truth[:, i]
+        p1 = prediction_param[:, i]
+        p2 = prediction_step[:, i]
+        p2_chopped = np.maximum(np.minimum(p2, 10), -10) # to keep it from diverging and messing up graphs
+        plt.figure()
+        plt.plot(p1, c='k', label='Prediction T-Param')
+        plt.plot(p2_chopped, c='b', label='Prediction 1 Steps')
+        plt.plot(gt, c='r', label='Groundtruth')
+        plt.legend()
+        plt.show()
+
+def plot_loss(training_error_t, training_error_no_t):
+    plt.figure()
+    plt.plot(np.array(training_error_t))
+    plt.title("Training Error with t")
+    plt.xlabel("epoch")
+    plt.ylabel("total loss")
+    plt.show()
+
+    plt.figure()
+    plt.plot(np.array(training_error_no_t))
+    plt.title("Training Error without t")
+    plt.xlabel("epoch")
+    plt.ylabel("total loss")
+    plt.show()
+
+def plot_loss_epoch(training_error_t, training_error_no_t, epochs):
+    plt.figure()
+    plt.bar(np.arange(epochs), np.array(training_error_t))
+    plt.title("Training Error with t")
+    plt.xlabel("epoch")
+    plt.ylabel("total loss")
+    plt.show()
+
+    plt.figure()
+    plt.bar(np.arange(epochs), np.array(training_error_no_t))
+    plt.title("Training Error without t")
+    plt.xlabel("epoch")
+    plt.ylabel("total loss")
+    plt.show()
+
+def plot_mse(mse_t, mse_no_t):
+    plt.figure()
+    plt.title("MSE over time for model with and without t")
+    plt.semilogy(mse_t, color='red', label='with t')
+    plt.semilogy(mse_no_t, color='blue', label='without t')
+    plt.legend()
+    plt.show()
+
+def test_models(traj, model, model_no_t):
+    log.info("Beginning testing of predictions")
+    mse_t = []
+    mse_no_t = []
+
+    # traj = test_data[0]
+    states = traj.states
+    actions = traj.actions
+    initial = states[0]
+    current = initial
+
+    predictions_t = [states[0,:]]
+    predictions_no_t = [states[0,:]]
+    for i in range(1, states.shape[0]):
+        pred_t = model.predict(np.hstack((initial, i, traj.P, traj.D, traj.target)))
+        pred_no_t = model_no_t.predict(np.concatenate((current, actions[i-1,:])))
+        predictions_t.append(pred_t.squeeze())
+        predictions_no_t.append(pred_no_t.squeeze())
+        groundtruth = states[i]
+        mse_t.append(np.square(groundtruth - pred_t).mean())
+        mse_no_t.append(np.square(groundtruth - pred_no_t).mean())
+        current = pred_no_t.squeeze()
+
+    return mse_t, mse_no_t, predictions_t, predictions_no_t
+
+###########################################
+#                 Misc                    #
+###########################################
 
 @hydra.main(config_path='conf/config.yaml')
 def contpred(cfg):
@@ -294,65 +374,17 @@ def contpred(cfg):
 
     # # Plot optimization NN
     if cfg.nn.training.plot_loss:
-        plt.figure()
-        plt.plot(np.array(logs.training_error))
-        plt.title("Training Error with t")
-        plt.xlabel("epoch")
-        plt.ylabel("total loss")
-        plt.show()
-
-        plt.figure()
-        plt.plot(np.array(logs_no_t.training_error))
-        plt.title("Training Error without t")
-        plt.xlabel("epoch")
-        plt.ylabel("total loss")
-        plt.show()
+        plot_loss(logs.training_error, logs_no_t.training_error)
 
     if cfg.nn.training.plot_loss_epoch:
-        plt.figure()
-        plt.bar(np.arange(cfg.nn.optimizer.epochs), np.array(logs.training_error_epoch))
-        plt.title("Training Error with t")
-        plt.xlabel("epoch")
-        plt.ylabel("total loss")
-        plt.show()
+        plot_loss_epoch(logs.training_error_epoch,
+            logs_no_t.training_error_epoch, cfg.nn.optimizer.epochs)
 
-        plt.figure()
-        plt.bar(np.arange(cfg.nn.optimizer.epochs), np.array(logs_no_t.training_error_epoch))
-        plt.title("Training Error without t")
-        plt.xlabel("epoch")
-        plt.ylabel("total loss")
-        plt.show()
+    mse_t, mse_no_t, predictions_t, predictions_no_t = test_models(test_data[0], model, model_no_t)
 
-    log.info("Beginning testing of predictions")
-    mse_t = []
-    mse_no_t = []
+    plot_states(test_data[0].states, np.array(predictions_t), np.array(predictions_no_t), idx_plot=[0, 1, 2, 3, 4, 5, 6])
 
-    traj = test_data[0]
-    states = traj.states
-    actions = traj.actions
-    initial = states[0]
-    current = initial
-
-    predictions_1 = [states[0,:]]
-    predictions_2 = [states[0,:]]
-    for i in range(1, states.shape[0]):
-        pred_t = model.predict(np.hstack((initial, i, traj.P, traj.D, traj.target)))
-        pred_no_t = model_no_t.predict(np.concatenate((current, actions[i-1,:])))
-        predictions_1.append(pred_t.squeeze())
-        predictions_2.append(pred_no_t.squeeze())
-        groundtruth = states[i]
-        mse_t.append(np.square(groundtruth - pred_t).mean())
-        mse_no_t.append(np.square(groundtruth - pred_no_t).mean())
-        current = pred_no_t.squeeze()
-
-    plot_states(states, np.array(predictions_1), np.array(predictions_2), idx_plot=[0, 1, 2, 3, 4, 5, 6])
-
-    plt.figure()
-    plt.title("MSE over time for model with and without t")
-    plt.semilogy(mse_t, color='red', label='with t')
-    plt.semilogy(mse_no_t, color='blue', label='without t')
-    plt.legend()
-    plt.show()
+    plot_mse(mse_t, mse_no_t)
 
     # Blocking this since it doesn't quite work
     if False:
@@ -457,38 +489,30 @@ def test_multiple_n_epochs(cfg):
     plot_states(states, np.array(predictions_t), np.array(predictions_no_t), idx_plot=[0, 1, 2, 3, 4, 5, 6])
 
 
-def plot_states(ground_truth, prediction_param, prediction_step, idx_plot=None, save=False):
-    num = np.shape(ground_truth)[0]
-    dx = np.shape(ground_truth)[1]
-    if idx_plot is None:
-        idx_plot = list(range(dx))
-
-    for i in idx_plot:
-        gt = ground_truth[:, i]
-        p1 = prediction_param[:, i]
-        p2 = prediction_step[:, i]
-        p2_chopped = np.maximum(np.minimum(p2, 10), -10) # to keep it from diverging and messing up graphs
-        plt.figure()
-        plt.plot(p1, c='k', label='Prediction T-Param')
-        plt.plot(p2_chopped, c='b', label='Prediction 1 Steps')
-        plt.plot(gt, c='r', label='Groundtruth')
-        plt.legend()
-        plt.show()
-
-
-def temp_generate_trajectories():
-    lengths = [10, 50, 100, 150, 200, 250, 300, 500]
-    for hor in lengths:
-        print("Generating length {} trajectories".format(hor))
-        data = np.array(collect_data(nTrials = 20, horizon=hor))
-        out = []
-        for trial in data:
-            out.extend(trial.states)
-        file = "trajectories/traj{}.npy".format(hor)
-        np.save(file, out)
 
 # train_data = collect_data(nTrials=50, horizon=300)  # 50
 # dataset = create_dataset_t_pid(train_data, probabilistic=True)
 if __name__ == '__main__':
     # sys.exit(test_multiple_n_epochs())
     sys.exit(contpred())
+
+
+###########################################
+#               Old Code                  #
+###########################################
+# def stateAction2forwardDyn(states, actions):
+#     data_in = np.concatenate((states[:-1, :], actions[:-1, :]), axis=1)
+#     data_out = states[1:, :]
+#     return [data_in, data_out]
+
+
+# def temp_generate_trajectories():
+#     lengths = [10, 50, 100, 150, 200, 250, 300, 500]
+#     for hor in lengths:
+#         print("Generating length {} trajectories".format(hor))
+#         data = np.array(collect_data(nTrials = 20, horizon=hor))
+#         out = []
+#         for trial in data:
+#             out.extend(trial.states)
+#         file = "trajectories/traj{}.npy".format(hor)
+#         np.save(file, out)
