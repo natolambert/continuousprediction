added normalization to dynamics_models and fixed bug with model configs

dynamics_model.py
@@ -8,6 +8,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
 from collections import OrderedDict
+from .mbrl_resources import ProbLoss
 
 
 class Net(nn.Module):
@@ -15,7 +16,7 @@ class Net(nn.Module):
     General Neural Network
     """
 
-    def __init__(self, n_in, n_out, cfg, loss_fn, tf=nn.ReLU()):
+    def __init__(self, n_in, n_out, cfg, loss_fn, tf=F.relu):
         """
         :param structure: layer sizes
         :param tf: nonlinearity function
@@ -26,15 +27,11 @@ class Net(nn.Module):
         self._onGPU = False
         self.loss_fn = loss_fn
 
-        self.n_in = n_in
-        self.n_out = n_out
-        self.hidden_w = cfg.model.training.hid_width
-
         # create object nicely
         layers = []
         layers.append(('dynm_input_lin', nn.Linear(self.n_in, self.hidden_w)))
         layers.append(('dynm_input_act', self.activation))
-        for d in range(cfg.model.training.hid_depth):
+        for d in range(cfg.model.depth):
             layers.append(('dynm_lin_' + str(d), nn.Linear(self.hidden_w, self.hidden_w)))
             layers.append(('dynm_act_' + str(d), self.activation))
 
@@ -42,65 +39,132 @@ class Net(nn.Module):
         self.features = nn.Sequential(OrderedDict([*layers]))
 
     def forward(self, x):
-        """
-        Runs a forward pass of x through this network
-        """
-        # TODO: to make it run I had the model call .float() on inputs, but this might affect performance
-        if type(x) == np.ndarray:
-            x = torch.from_numpy(x)
-        x = self.features(x.float())
+        x = self.features(x)
         return x
 
-    def optimize(self, dataset, cfg):
-        """
-        Uses dataset to train this net according to the parameters in cfg
+    def testPreprocess(self, input):
+        if(input.shape[1] == 26):
+            inputStates = input[:, :21]
+            inputActions = input[:, 21:]
+            normStates = self.stateScaler.transform(inputStates)
+            normActions = self.actionScaler.transform(inputActions)
+            return np.hstack((normStates, normActions))
+        elif(input.shape[1] == 37):
+            inputStates = input[:, :21]
+            inputIndex = input[:, 21]
+            inputP = input[:, 22:27]
+            inputD = input[:, 27:32]
+            inputTargets = input[:, 32:]
+            normStates = self.stateScaler.transform(inputStates)
+            normIndex = self.indexScaler.transform(inputIndex)
+            normP = self.PScaler.transform(inputP)
+            normD = self.DScaler.transform(inputD)
+            normTargets = self.targetScaler.transform(inputTargets)
+            return np.hstack((normStates, normIndex, normP, normD, normTargets))
+        else:
+            print("Incorrect dataset length, no normalization performed")
+            return input
+    def testPostprocess(self, output):
+        return self.outputScaler.inverse_transform(output)
 
-        Returns:
-            train_errors: a list of average errors for each epoch on the training data
-            test_errors: a list of average errors for each epoch on the test data
-        """
+    def preprocess(self, dataset, cfg):
+
+        # Select scaling, minmax vs standard (fits to a gaussian with unit variance and 0 mean)
+        # TODO: Selection should be in config
+        # StandardScaler, MinMaxScaler
+        # TODO: Instead of hardcoding, include in config, trajectory vs. one-step, length of different inputs, etc.
+        # 26 -> one-step, 37 -> trajectory
+        input = dataset[0]
+        output = dataset[1]
+        if(input.shape[1] == 26):
+            self.stateScaler = hydra.utils.instantiate(cfg.model.preprocess.state)
+            self.actionScaler = hydra.utils.instantiate(cfg.model.preprocess.action)
+            self.outputScaler = hydra.utils.instantiate(cfg.model.preprocess.output)
+
+            inputStates = input[:, :21]
+            inputActions = input[:, 21:]
+
+            stateScaler.fit(inputStates)
+            actionScaler.fit(inputActions)
+            outputScaler.fit(output)
+
+            normStates = self.stateScaler.transform(inputStates)
+            normActions = self.actionScaler.transform(inputActions)
+            normOutput = self.outputScaler.transform(output)
+            normInput = np.hstack((normStates, normActions))
+            return list(zip(normInput, normOutput))
+        elif(input.shape[1] == 37):
+            self.stateScaler = hydra.utils.instantiate(cfg.model.preprocess.state)
+            self.indexScaler = hydra.utils.instantiate(cfg.model.preprocess.index)
+            self.PScaler = hydra.utils.instantiate(cfg.model.preprocess.P)
+            self.DScaler = hydra.utils.instantiate(cfg.model.preprocess.D)
+            self.targetScaler = hydra.utils.instantiate(cfg.model.preprocess.target)
+            self.outputScaler = hydra.utils.instantiate(cfg.model.preprocess.output)
+
+            inputStates = input[:, :21]
+            inputIndex = input[:, 21]
+            inputP = input[:, 22:27]
+            inputD = input[:, 27:32]
+            inputTargets = input[:, 32:]
+
+            stateScaler.fit(inputStates)
+            indexScaler.fit(inputIndex)
+            PScaler.fit(inputP)
+            DScaler.fit(inputD)
+            targetScaler.fit(inputTargets)
+            outputScaler.fit(output)
+
+            normStates = self.stateScaler.transform(inputStates)
+            normIndex = self.indexScaler.transform(inputIndex)
+            normP = self.PScaler.transform(inputP)
+            normD = self.DScaler.transform(inputD)
+            normTargets = self.targetScaler.transform(inputTargets)
+            normOutput = self.outputScaler.transform(output)
+            normInput = np.hstack((normStates, normIndex, normP, normD, normTargets))
+            return list(zip(normInput, normOutput))
+        else:
+            print("Incorrect dataset length, no normalization performed")
+            return list(zip(input, output))
+
+    def optimize(self, dataset, cfg):
         from torch.utils.data import DataLoader
 
-        # Extract parameters from cfg
+        # This bit basically adds variables to the dotmap with default values
         lr = cfg.model.optimizer.lr
         bs = cfg.model.optimizer.batch
         split = cfg.model.optimizer.split
         epochs = cfg.model.optimizer.epochs
-
-        # Set up the optimizer and scheduler
-        # TODO: the scheduler is currently unused. Should it be doing something it isn't or removed?
-        optimizer = torch.optim.Adam(self.features.parameters(), lr=lr)
+        # Optimizer
+        optimizer = torch.optim.Adam(super(DynamicsModel, self).parameters(), lr=lr)
+        # optimizer = torch.optim.SGD(super(GeneralNN, self).parameters(), lr=lr)
         scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=6, gamma=0.7)
 
+        # data preprocessing for normalization
+        dataset = self.preprocess(dataset, cfg)
+
         # Puts it in PyTorch dataset form and then converts to DataLoader
-        dataset = list(zip(dataset[0], dataset[1]))
         trainLoader = DataLoader(dataset[:int(split * len(dataset))], batch_size=bs, shuffle=True)
         testLoader = DataLoader(dataset[int(split * len(dataset)):], batch_size=bs, shuffle=True)
 
-        # Optimization loop
         train_errors = []
         test_errors = []
         for epoch in range(epochs):
-            print("    Epoch %d" % (epoch+1))
-
             train_error = 0
             test_error = 0
-
-            # Iterate through dataset and take gradient descent steps
+            # log.info("Epoch %d" % (epoch))
             for i, (inputs, targets) in enumerate(trainLoader):
                 optimizer.zero_grad()
                 outputs = self.forward(inputs)
-                loss = self.loss_fn(outputs.float(), targets.float())
+                loss = self.loss_fn(outputs, targets)
                 train_error += loss.item() / (len(trainLoader) * bs)
 
                 loss.backward()
                 optimizer.step()  # Does the update
 
-            # Iterate through dataset to calculate test set accuracy
             test_error = torch.zeros(1)
             for i, (inputs, targets) in enumerate(testLoader):
                 outputs = self.forward(inputs)
-                loss = self.loss_fn(outputs.float(), targets.float())
+                loss = self.loss_fn(outputs, targets)
                 test_error += loss.item() / (len(testLoader) * bs)
 
             train_errors.append(train_error)
@@ -110,12 +174,6 @@ class Net(nn.Module):
 
 
 class DynamicsModel(object):
-    """
-    Wrapper class for a general dynamics model.
-
-    The model is an ensemble of neural nets. For cases where the model should not be an ensemble it is just
-    an ensemble of 1 net.
-    """
     def __init__(self, cfg):
         self.ens = cfg.model.ensemble
         self.traj = cfg.model.traj
@@ -140,23 +198,17 @@ class DynamicsModel(object):
         else:
             self.loss_fn = nn.MSELoss()
 
-        self.nets = [Net(self.n_in, self.n_out, cfg, self.loss_fn) for i in range(self.E)]
+        self.nets = [Net(self.n_in, self.n_out, cfg, self.loss_fn) for i in self.E]
 
     def predict(self, x):
-        """
-        Use the model to predict values with x as input
-        TODO: Fix hardcoding in this method
-        TODO: particle sampling approach for probabilistic model
-        """
-        if type(x) == np.ndarray:
-            x = torch.from_numpy(x)
-        prediction = torch.zeros((x.shape[0], self.n_out))
+        prediction = torch.zeros(self.n_out)
         for n in self.nets:
-            prediction += n.forward(x) / len(self.nets)
+            scaledInput = n.testPreprocess(x)
+            prediction += n.testPostprocess(n.forward(x)) / len(self.nets)
         if self.traj:
-            return prediction[:,:21]
+            return prediction
         else:
-            return x[:,:21] + prediction[:,:21]
+            return x + prediction
 
     def train(self, dataset, cfg):
         acctest_l = []
@@ -171,9 +223,9 @@ class DynamicsModel(object):
 
             # iterate through the validation sets
             for (i, n), (train_idx, test_idx) in zip(enumerate(self.nets), kf.split(dataset[0])):
-                print("  Model %d" % (i+1))
                 # only train on training data to ensure diversity
-                sub_data = (dataset[0][train_idx], dataset[1][train_idx])
+                raise NotImplementedError("Fix K-folding")
+                sub_data = dataset[train_idx]
                 train_e, test_e = n.optimize(sub_data, cfg)
                 acctrain_l.append(train_e)
                 acctest_l.append(test_e)
@@ -182,44 +234,4 @@ class DynamicsModel(object):
             acctrain_l.append(train_e)
             acctest_l.append(test_e)
 
-        self.acctrain, self.acctest = acctrain_l, acctest_l
-
         return acctrain_l, acctest_l
-
-class ProbLoss(nn.Module):
-    """
-    Class for probabilistic loss function
-    """
-
-    def __init__(self, size):
-        super(ProbLoss, self).__init__()
-        self.size = size
-        self.max_logvar = torch.nn.Parameter(
-            torch.tensor(1 * np.ones([1, size]), dtype=torch.float, requires_grad=True))
-        self.min_logvar = torch.nn.Parameter(
-            torch.tensor(-1 * np.ones([1, size]), dtype=torch.float, requires_grad=True))
-
-    def softplus_raw(self, input):
-        # Performs the elementwise softplus on the input
-        # softplus(x) = 1/B * log(1+exp(B*x))
-        B = torch.tensor(1, dtype=torch.float)
-        return (torch.log(1 + torch.exp(input.mul_(B)))).div_(B)
-
-        # TODO: This function has been observed outputting negative values. needs fix
-
-    def forward(self, inputs, targets):
-        # size = targets.size()[1]
-        mean = inputs[:, :self.size]
-        logvar = inputs[:, self.size:]
-
-        # Caps max and min log to avoid NaNs
-        logvar = self.max_logvar - self.softplus_raw(self.max_logvar - logvar)
-        logvar = self.min_logvar + self.softplus_raw(logvar - self.min_logvar)
-
-        var = torch.exp(logvar)
-
-        diff = mean - targets
-        mid = diff / var
-        lg = torch.sum(torch.log(var))
-        out = torch.trace(torch.mm(diff, mid.t())) + lg
-        return out