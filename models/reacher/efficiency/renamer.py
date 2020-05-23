b170aea336d8d356d0262d9883985ae91b032f06
Albert Wilcox <albertwilcoxiii@gmail.com>
2020-05-22 20:14:24 -0700

    models nut not efficiency models

--- models/reacher/efficiency/renamer.py
+++
@@ -1,5 +0,0 @@
-import os
-
-for type in ['d', 't', 'de', 'te', 'p', 'tp', 'pe', 'tpe']:
-    for num_traj in [1, 2, 5, 10, 20, 50, 100]:
-        os.rename('%s/n%d_t1.dat' % (type, num_traj), '%s/n%d_t500.dat' % (type, num_traj))
